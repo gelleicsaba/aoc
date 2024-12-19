506deeff3c7ff8dee6da4555f495709605718eb3
Gellei Csaba <gellei.csaba@gmail.com>
2024-12-19 07:41:43 +0100

    241219

--- 2024/lib/aoclib.ts
+++ 2024/lib/aoclib.ts
@@ -29,3 +29,15 @@ export const permutations = (arr: any[]) => {
 export const tupleToArray=(s:string,mapFunc?: Function)=>
     mapFunc ? s.substring(1,s.length-1).split(',').map(v=>mapFunc(v))
         : s.substring(1,s.length-1).split(',')
+
+export const xor=(b:number,c:number):number=>{
+    let b_=b.toString(2).padStart(50,'0')
+    let c_=c.toString(2).padStart(50,'0')
+    let d=0
+    let n=1
+    for (let x=49;x>=0;--x) {
+        if ((b_[x]=='0'&&c_[x]=='1')||(b_[x]=='1'&&c_[x]=='0')) d+=n
+        n*=2
+    }
+    return d
+}
--- 2024/lib/xor.ts
+++ 2024/lib/xor.ts
@@ -1,9 +1,11 @@
 export const xor=(b:number,c:number):number=>{
-    let b_=b.toString(2).padStart(50,'0').split('')
-    let c_=c.toString(2).padStart(50,'0').split('')
-    let d=Array(50)
-    for (let x=0;x<50;++x) {
-      d[x]=(b_[x]=='0'&&c_[x]=='1')||(b_[x]=='1'&&c_[x]=='0')?'1':'0'
-    }
-    return parseInt(d.join(''),2)
+  let b_=b.toString(2).padStart(50,'0')
+  let c_=c.toString(2).padStart(50,'0')
+  let d=0
+  let n=1
+  for (let x=49;x>=0;--x) {
+      if ((b_[x]=='0'&&c_[x]=='1')||(b_[x]=='1'&&c_[x]=='0')) d+=n
+      n*=2
+  }
+  return d
 }
