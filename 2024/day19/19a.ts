c23f1b29e246322a188a6320adde3a637c9b5b60
Gellei Csaba <gellei.csaba@gmail.com>
2024-12-21 11:09:20 +0100

    241221

--- 2024/day19/19a.ts
+++ 2024/day19/19a.ts
@@ -1,71 +1,48 @@
 export {}
 import { input } from './19data'
-import { searchAll } from '../lib/aoclib'
 const mutate=(s:string):string[]=>s.replaceAll("\r","").split("\n")
-const test=
-`r, wr, b, g, bwu, rb, gb, br
-
-brwrr
-bggr
-gbbr
-rrbgbr
-ubwu
-bwurrg
-brgr
-bbrgwb`
-
 // const test=
-// `ac, cab, c
+// `r, wr, b, g, bwu, rb, gb, br
 
-// caccab
-// acab`
-
-
+// brwrr
+// bggr
+// gbbr
+// rrbgbr
+// ubwu
+// bwurrg
+// brgr
+// bbrgwb`
 // const _input=mutate(test)
 const _input=mutate(input)
-
 const samples=_input[0].split(',').map(v=>v.trim()).sort((a,b)=>b.length-a.length)
 const towels: string[]=[]
 for (let x=2;x<_input.length;++x) {
     towels.push(_input[x])
 }
-
-
 let currentTowel=""
 let currentLen=0
 let solutionResult=false
 let currentSamples:string[]=[]
 let calls=0
 const findSolution=(startIdx: number)=>{
-    // console.log(`${startIdx} ${calls}`)
     ++calls
     for (let x of currentSampleCache[startIdx]) {
         if (x.length==0) continue
         if (solutionResult || calls>500000000) return
-        // if (startIdx+x.length-1<currentLen &&
-        //     currentTowelCache[startIdx][x.length]==x)
-        // {
-            // console.log(`OK${startIdx}`)
-
-            if (startIdx+x.length==currentLen)
-            {
-                solutionResult=true
-                return
-            }
-            findSolution(startIdx+x.length)
-
-        // }
+        if (startIdx+x.length==currentLen)
+        {
+            solutionResult=true
+            return
+        }
+        findSolution(startIdx+x.length)
     }
 }
-
-
 let samplesMaxLen=0
 for (let x=0;x<samples.length;++x) {
     if (samples[x].length>samplesMaxLen) {
         samplesMaxLen=samples[x].length
     }
 }
-
 let sum=0
 let currentTowelCache: any[]=[]
 let currentSampleCache: any[]=[]
@@ -97,9 +74,6 @@ for (let x=0;x<towels.length;++x) {
             }
         }
     }
-    // console.log(currentTowelCache)
-    // console.log(currentSampleCache)
-    // break
     calls=0
     findSolution(0)
     process.stdout.write(`(${solutionResult})  `)
