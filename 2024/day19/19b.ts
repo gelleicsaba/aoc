72c6d1124247c8432fe2056388823b43133234f5
Gellei Csaba <gellei.csaba@gmail.com>
2024-12-23 11:19:36 +0100

    241223

--- 2024/day19/19b.ts
+++ 2024/day19/19b.ts
@@ -3,26 +3,14 @@ import { input } from './19data'
 import cloneDeep from 'clonedeep'
 import { existFile, readObjFromFile, writeObjToFile } from '../lib/aoclib'
 const VALID_INPUTS_FILE = './validInputs.txt'
-const mutate=(s:string):string[]=>s.replaceAll("\r","").split("\n")
-// const test=
-// `r,b,g,br,gb,rb
-
-// rrbgbr`
-// const _input=mutate(test)
-
 if (!existFile(VALID_INPUTS_FILE)) {
     console.log("THE 'validInputs.json' DOESN'T EXIST!")
     console.log("RUN THE '19a.ts' (TASK A) TO GET VALID INPUTS!")
     process.exit(0)
 }
-const _input=mutate(input)
-let samples: string[]=[]
-let towels: string[]=[]
-
 const result19a:any=readObjFromFile(VALID_INPUTS_FILE)
-towels=result19a.towels
-samples=result19a.samples
-
+let towels: string[]=result19a.towels
+let samples: string[]=result19a.samples
 let _samples:string[]=[]
 let scores={}
 let cache={}
@@ -50,8 +38,6 @@ const _build=(s:string, nodes:string[])=>{
     }
 }
 const build=(idx: number)=>{
-
-    console.log(`build ${idx}.`)
     scores={}
     cache={}
     _samples=samples.filter(v=>towels[idx].includes(v))
@@ -61,11 +47,9 @@ let sum=0
 for (let x=0;x<towels.length;++x) {
     build(x)
     sum+=scores[towels[x]]
-    console.log(`  ${x}: ${scores[towels[x]]}`)
 }
 console.log("DON'T FORGET!")
 console.log("IF YOUR INPUT HAS CHANGED, RUN TASK A (19a.ts) AGAIN!!!\n")
-console.log()
 console.log(`B: ${sum}`)
 
 
