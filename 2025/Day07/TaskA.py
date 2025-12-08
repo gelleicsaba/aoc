75d5400884539eabcf4fe3522308638162def64c
Gellei Csaba <gellei.csaba.bnet@gmail.com>
2025-12-08 20:13:04 +0100

    2025/7/1

--- 2025/Day07/TaskA.py
+++ 2025/Day07/TaskA.py
@@ -4,17 +4,14 @@ m = []
 for row in textContent.split("\n"):
     m.append(list(row))
 splitNum = 0
-def scan(r):
-    global splitNum
+for r in range(len(m)-2):
     for t in range(len(m[0])):
-        if m[r-1][t] == 'S':
-            m[r][t] = '|'
-        elif m[r-1][t] == '|' and m[r][t] == '.':
-            m[r][t] = '|'
-        elif m[r-1][t] == '|' and m[r][t] == '^':
-            m[r][t-1] = '|'
-            m[r][t+1] = '|'
+        if m[r][t] == 'S':
+            m[r+1][t] = '|'
+        elif m[r][t] == '|' and m[r+1][t] == '.':
+            m[r+1][t] = '|'
+        elif m[r][t] == '|' and m[r+1][t] == '^':
+            m[r+1][t-1] = '|'
+            m[r+1][t+1] = '|'
             splitNum = splitNum + 1
-for y in range(len(m)-2):
-    scan(y+1)
 print(splitNum)
